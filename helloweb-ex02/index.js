561fa765fe89714302fcbd1ba3e85b652dda1bf3
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-03-04 09:50:12 +0900

    express #1

--- helloweb-ex02/index.js
+++ helloweb-ex02/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const express = require('express');
 
 const helloRouter = require('./routes/hello');
-const { nextTick } = require('process');
+const mainRouter = require('./routes/main');
+const userRouter = require('./routes/user');
 
 const port = 9090;
 
@@ -12,15 +13,19 @@ const application = express()
     // 1. static resources
     .use(express.static(path.join(__dirname, "assets")))
     // 2. request body parser
-    .use(express.urlencoded({extended: true}))    // application/x-www-form-urlencoded
-    .use(express.json())        // application/json 
+    .use(express.urlencoded({extended: true}))     // application/x-www-form-urlencoded
+    .use(express.json())                           // application/json 
     // 3. view engine
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'ejs')
     // 4. request router
     .all('*', function(req, res, next){
         res.locals.req = req;
         res.locals.res = res;
         next();
     })
+    .use('/', mainRouter)
+    .use('/user', userRouter)
     .use('/hello', helloRouter); 
 
 // Server SetUp
